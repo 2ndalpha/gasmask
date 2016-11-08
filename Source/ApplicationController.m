8e981d2c8fce1eb07d51f237efdec9ada34c544c
Andrew Muraco <andrew.muraco@synacor.com>
2016-11-07 21:40:26 -0500

    Changed how the show/hide editor window logic works so that relaunching is not necessary. Bumped version to 0.8.5

--- Source/ApplicationController.m
+++ Source/ApplicationController.m
@@ -110,9 +110,7 @@ static ApplicationController *sharedInstance = nil;
 
 - (IBAction)closeEditorWindow:(id)sender
 {
-	[Preferences setShowEditorWindow:NO];
-	shouldQuit = NO;
-	[self quit:nil];
+	[self hideApplicationFromDock];
 }
 
 - (IBAction)addFromURL:(id)sender
@@ -203,8 +201,9 @@ static ApplicationController *sharedInstance = nil;
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication
 {
-	shouldQuit = NO;
-	return YES;
+	shouldQuit = YES;
+	[self hideApplicationFromDock];
+	return NO;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification
@@ -292,25 +291,38 @@ static ApplicationController *sharedInstance = nil;
     [NotificationHelper notify:@"Hosts File Activated" message:[hosts name]];
 }
 
+BOOL tranformAppToState(ProcessApplicationTransformState newState)
+{
+	ProcessSerialNumber psn = { 0, kCurrentProcess };
+	OSStatus transformStatus = TransformProcessType(&psn, newState);
+	if((transformStatus != 0))
+	{
+		NSError *error = [NSError errorWithDomain:NSOSStatusErrorDomain code:transformStatus userInfo:nil];
+		NSLog(@"TranformAppToState: Unable to transform App state. Error - %@",error);
+	}
+
+	return (transformStatus == 0);
+}
+
 - (void)showApplicationInDock
 {
-	ProcessSerialNumber psn = {0, kCurrentProcess};
-	OSStatus returnCode = TransformProcessType(&psn, kProcessTransformToForegroundApplication);
-	if (returnCode == 0) {
+	BOOL bSuccess = tranformAppToState(kProcessTransformToForegroundApplication);
+	if(bSuccess)
+	{
+		[NSApp activateIgnoringOtherApps:YES];
 		ProcessSerialNumber psnx = {0, kNoProcess};
 		GetNextProcess(&psnx);
 		SetFrontProcess(&psnx);
 		[self performSelector:@selector(setFront) withObject:nil afterDelay:0.5];
 	}
+
 }
 
 - (void)hideApplicationFromDock
 {
-	NSArray *arguments = [NSArray arrayWithObjects:@"-c",
-						  [NSString stringWithFormat:@"sleep 1 ; /usr/bin/open '%@' --args \\#reopen\\#", [[NSBundle mainBundle] bundlePath]],
-						  nil];
-	[NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:arguments];
-	[NSApp terminate:self];
+	tranformAppToState(kProcessTransformToBackgroundApplication);
+	[Preferences setShowEditorWindow:NO];
+	editorWindowOpened = NO;
 }
 
 - (void)setFront
