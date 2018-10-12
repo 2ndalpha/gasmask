7efb437f5dd91187880bf05123aa20f9bd10e46a
Raymond Sneekes <raymond@sneekes.com>
2018-10-12 20:21:08 +0200

    Use NSURLSessionDataTask to download remote data
    It seemed [NSStream getStreamsToHost] did not implement https/ssl

--- Source/HostsDownloader.m
+++ Source/HostsDownloader.m
@@ -25,8 +25,8 @@
 #import "ExtendedNSApplication.h"
 
 #define kBufferSize 1024
-#define kHeaderLastModified @"Last-Modified:"
-#define kHeaderContentType @"Content-Type:"
+#define kHeaderLastModified @"Last-Modified"
+#define kHeaderContentType @"Content-Type"
 #define kContentTypeHtml @"text/html"
 #define kContentTypeImage @"image"
 #define kContentTypeText @"text/plain"
@@ -38,8 +38,6 @@
 
 @interface HostsDownloader (Private)
 
-- (void)sendRequestHeader;
-- (void)readResponse;
 - (void)notifyDelegateHostsUpToDate;
 - (void)notifyDelegateDownloadingStarted;
 - (void)notifyDelegateDownloaded;
@@ -68,8 +66,6 @@
 	hosts = hostsValue;
 	url = urlValue;
 	
-	headerRead = NO;
-	
 	return self;
 }
 
@@ -80,215 +76,68 @@
 
 - (void)download
 {
-	dispatch_async(dispatch_get_global_queue(0, 0), ^{
-		
-		logDebug(@"Downloading: %@", url);
-		
-		response = [NSMutableString string];
-		
-		NSHost *host = [NSHost hostWithName:[url host]];
-		
-		NSNumber *port = [url port];
-		if (port == nil) {
-			port = kDefaultPort;
-		}
-        
-        NSInputStream *newInputStream;
-        NSOutputStream *newOutputStream;
-		
-		[NSStream getStreamsToHost:host port:[port intValue] inputStream:&newInputStream outputStream:&newOutputStream];
-		
-		// Failed to open connection
-		if (newOutputStream == nil || newOutputStream == nil) {
-			error = [[Error alloc] initWithType:ServerNotFound];
-			[self notifyDelegateDownloadFailed];
-			return;
-		}
-        
-        inputStream = newInputStream;
-        outputStream = newOutputStream;
-        
-		[inputStream setDelegate:self];
-		[outputStream setDelegate:self];
-		
-		dispatch_async(dispatch_get_main_queue(), ^{
-			[inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop]
-								   forMode:NSDefaultRunLoopMode];
-			[outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop]
-									forMode:NSDefaultRunLoopMode];
-			
-			[inputStream open];
-			[outputStream open];
-		});
-        
-	});
-}
+    logDebug(@"Downloading: %@", url);
+    
+    NSURLSessionDataTask *task = [
+          NSURLSession.sharedSession dataTaskWithURL:url
+          completionHandler:^(NSData *data, NSURLResponse *urlResponse, NSError *taskError) {
+              if (error || data.length == 0) {
+                  error = [[Error alloc] initWithType:FailedToDownload];
+                  NSString *description = @"Failed to download the hosts file";
+                  [error setDescription:description];
+                  [error setUrl:url];
+                  
+                  [self notifyDelegateDownloadFailed];
+                  return;
+              }
+              
+              NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)urlResponse;
+              lastModified = [[httpResponse allHeaderFields] objectForKey:kHeaderLastModified];
+              
+              // Check If Hosts File Is Up To Date
+              if ([hosts isKindOfClass:[RemoteHosts class]] && [lastModified isEqual:[(RemoteHosts*)hosts lastModified]]) {
+                  upToDate = YES;
+                  [self notifyDelegateHostsUpToDate];
+                  return;
+              }
+              
+              contentType = [[httpResponse allHeaderFields] objectForKey:kHeaderContentType];
+              
+              if (![contentType hasPrefix:kContentTypeText])
+              {
+                  if ([contentType hasPrefix:kContentTypeHtml]) {
+                      [self addBadContentTypeError:@"Can't download the hosts file. It contains HTML page."];
+                  }
+                  else if ([contentType hasPrefix:kContentTypeImage]) {
+                      [self addBadContentTypeError:@"Can't download the hosts file. It contains image."];
+                  }
+                  else {
+                      logDebug(@"Content type: %@", contentType);
+                      [self addUnknownContentTypeError];
+                  }
+                  [self notifyDelegateDownloadFailed];
+                  return;
+              }
+              
+              NSString * output = [[NSString alloc] initWithData:data
+                                                        encoding:NSUTF8StringEncoding];
+              response = [NSMutableString string];
+              [response appendString:output];
+              
+              if ([response isEqual:[hosts contents]]) {
+                  [self notifyDelegateHostsUpToDate];
+              } else {
+                  [self notifyDelegateDownloaded];
+              }
+          }];
 
-#pragma mark -
-#pragma mark NSStreamDelegate
-
-- (void)stream:(NSStream *)stream handleEvent:(NSStreamEvent)eventCode
-{
-	switch (eventCode) {
-		case NSStreamEventHasSpaceAvailable:
-			if (stream == outputStream) {
-				[self sendRequestHeader];
-			}
-			break;
-		case NSStreamEventHasBytesAvailable:
-			if (stream == inputStream) {
-				[self readResponse];
-				
-				if (upToDate) {
-					[self notifyDelegateHostsUpToDate];
-				}
-				else if (error) {
-					[self notifyDelegateDownloadFailed];
-				}
-			}
-			break;
-		case NSStreamEventEndEncountered:
-			if (stream == inputStream) {
-				if ([response isEqual:[hosts contents]]) {
-					[self notifyDelegateHostsUpToDate];
-				}
-				else {
-					[self notifyDelegateDownloaded];
-				}
-			}
-			break;
-		case NSStreamEventErrorOccurred: {
-			
-			error = [[Error alloc] initWithType:FailedToDownload];
-			NSString *description = @"Failed to download the hosts file";
-			[error setDescription:description];
-			[error setUrl:url];
-			
-			[inputStream close];
-			[outputStream close];
-			
-			[self notifyDelegateDownloadFailed];
-			break;
-        }
-        default:
-            break;
-	}
+    [self notifyDelegateDownloadingStarted];
+    [task resume];
 }
 
 @end
 
 @implementation HostsDownloader (Private)
-
-- (void)sendRequestHeader
-{
-	logDebug(@"Sending request");
-	
-	NSMutableString *request = [NSMutableString new];
-	[request appendFormat:@"GET %@ HTTP/1.0\r\n", [[url path] length] ? [url path] : @"/"];
-	[request appendFormat:@"Host: %@\r\n", [url host]];
-	[request appendFormat:@"User-Agent: Gas Mask/%@\r\n", [NSApplication version]];
-	[request appendString:@"Accept: text/html,text/plain\r\n"];
-	[request appendString:@"\r\n"];
-	
-	const uint8_t * raw = (const uint8_t *)[request UTF8String];
-	[outputStream write:raw maxLength:[request length]];
-	[outputStream close];
-}
-
-- (void)readResponse
-{	
-	uint8_t buf[kBufferSize];
-	unsigned int len = 0;
-	len = [inputStream read:buf maxLength:kBufferSize];
-
-	if (len) {
-		NSString *output = [[NSString alloc] initWithBytes:buf length:len encoding:NSASCIIStringEncoding];
-		
-		if (!headerRead) {
-			NSArray *lines = [output componentsSeparatedByString:LINE_END];
-			
-			for (int i=0; i<[lines count]; i++) {
-				NSString *line = [lines objectAtIndex:i];
-				
-				// End of the header
-				if (!headerRead && [line length] == 0) {
-					
-					headerRead = YES;
-					[self notifyDelegateDownloadingStarted];
-					
-					// Page did not specify content type - cancel download
-					if (contentType == nil) {
-						[self addUnknownContentTypeError];
-						[inputStream close];
-						[outputStream close];
-						return;
-					}
-					
-					continue;
-				}
-				
-				if (headerRead) {
-					[response appendString:line];
-					if (i != [lines count]-1 || [output hasSuffix:LINE_END]) {
-						[response appendString:LINE_END];
-					}
-				}
-				else {
-					if ([line hasPrefix:kHeaderLastModified]) {
-						lastModified = [line substringFromIndex:[kHeaderLastModified length]];
-						
-						// Hosts File Is Up To Date
-						if ([hosts isKindOfClass:[RemoteHosts class]] && [lastModified isEqual:[(RemoteHosts*)hosts lastModified]]) {
-							[inputStream close];
-							[outputStream close];
-							upToDate = YES;
-							return;
-						}
-					}
-					else if ([line hasPrefix:kHeaderContentType]) {
-						contentType = [line substringFromIndex:[kHeaderContentType length]];
-						contentType = [contentType trim];
-						
-						if (![contentType hasPrefix:kContentTypeText]) {
-							if ([contentType hasPrefix:kContentTypeHtml]) {
-								[self addBadContentTypeError:@"Can't download the hosts file. It contains HTML page."];
-							}
-							else if ([contentType hasPrefix:kContentTypeImage]) {
-								[self addBadContentTypeError:@"Can't download the hosts file. It contains image."];
-							}
-							else {
-								logDebug(@"Content type: %@", contentType);
-								[self addUnknownContentTypeError];
-							}
-						}
-					}
-					else if ([line hasPrefix:HTTP_1_0] || [line hasPrefix:HTTP_1_1]) {
-						NSRange range = NSMakeRange([HTTP_1_0 length], 3);
-						NSString *rawResponseCode = [line substringWithRange:range];
-						int responseCode = [rawResponseCode intValue];
-						if (responseCode >= 400) {
-							error = [[Error alloc] initWithType:FileNotFound];
-							NSString *description = [NSString stringWithFormat:
-													 @"Failed to download the hosts file.\nRemote server responded with error code \"%@\".",
-													 rawResponseCode];
-							[error setDescription:description];
-							[error setUrl:url];
-						}
-					}
-				}
-				
-				if (error) {
-					[inputStream close];
-					[outputStream close];
-					return;
-				}
-			}
-		}
-		else {
-			[response appendString:output];
-		}
-	}
-}
 
 - (void)notifyDelegateHostsUpToDate
 {
