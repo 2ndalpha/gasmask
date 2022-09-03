1ff968994f0844e6d363713e54033007636197d9
Siim Raud <siim@qminder.com>
2022-09-03 18:07:46 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -eu
 
-# Workaround for a Cocoapods 1.5 bug: https://github.com/CocoaPods/CocoaPods/issues/7708
-export EXPANDED_CODE_SIGN_IDENTITY="-"
-export EXPANDED_CODE_SIGN_IDENTITY_NAME="-"
-
-pod install
 xcodebuild -workspace "Gas Mask.xcworkspace" -scheme "Gas Mask" ARCHS="i386 x86_64" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
