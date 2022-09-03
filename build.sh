3f7dff7871bd5c23090d56f050027a954ad507a4
Siim Raud <siim@qminder.com>
2022-09-03 18:09:34 +0300

    Updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-xcodebuild -workspace "Gas Mask.xcworkspace" -scheme "Gas Mask" ARCHS="i386 x86_64" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -project "Gas Mask.xcodeproj" -scheme "Gas Mask" ARCHS="i386 x86_64" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
