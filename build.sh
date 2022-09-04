75eded84c50dedd1049324f825235043face8651
ec2-user <ec2-user@ip-172-31-47-197.ec2.internal>
2022-09-04 15:24:11 +0000

    feat: build for ARM

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eu
 
-xcodebuild -project "Gas Mask.xcodeproj" -scheme "Gas Mask" ARCHS="i386 x86_64" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild -project "Gas Mask.xcodeproj" -scheme "Gas Mask" ARCHS="arm64 x86_64" CODE_SIGN_IDENTITY="" 
+CODE_SIGNING_REQUIRED=NO
